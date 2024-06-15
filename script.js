fa55e237e56e5640f39a99f606dfe411934c1553
sardor <alimukhammad1@gmail.com>
2024-06-14 22:02:41 -0400

    2

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  let submit = document.getElementById("submitId");
+  let failed = document.getElementById("failedId");
+  let info = document.getElementById("infoId");
+  let warning = document.getElementById("warningId");
+
+
+  
+}
